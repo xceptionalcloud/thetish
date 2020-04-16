8c405ac1597a6e7f7ec3e9c3a286513effc2b78c
xndev <alangford@xceptional.com>
2020-04-15 17:49:19 -0700

    Updates

---
+++ single_ticket.py
@@ -0,0 +1,45 @@
+import requests
+import configparser
+import base64
+
+config = configparser.RawConfigParser()
+config.read('/var/www/settings.ini')
+cwaClientId = config['CW']['clientid']
+cwaAuth = config['CW']['auth']
+cwaUrlPre = 'https://api-na.myconnectwise.net/v4_6_release/apis/3.0/'
+cwaApiType = 'company/companies/'
+cwaApiType = 'sales/opportunities/'
+cwaApiType = 'service/boards/'
+cwaApiType = 'service/tickets/'
+cwaTicketId = '64418'
+cwaApiCall = ''
+cwaApiParamPre = '?'
+cwaPages = [1, 2, 3, 4, 5, 6]
+pageSize = 500
+response = ''
+
+for page in cwaPages:
+    iterator = 0
+    # cwaApiParams = {'pageSize': str(pageSize), 'page': str(page)}
+    # cwaApiParamPkg = ''
+    # for entry, value in cwaApiParams.items():
+        # iterator+=1
+        # if iterator == len(cwaApiParams):
+            # cwaApiParamPkg = str(cwaApiParamPkg) + str(entry) + '=' + str(value)
+        # else:
+            # cwaApiParamPkg = str(cwaApiParamPkg) + str(entry) + '=' + str(value) + '&'
+    # cwaApiParamFinal = cwaApiParamPre + cwaApiParamPkg
+    cwaApiUrl = cwaUrlPre + cwaApiType + cwaTicketId
+    print(cwaApiUrl)
+    cwaOppReq = requests.get(str(cwaApiUrl), headers={'Authorization': str(cwaAuth), 'Accept': 'application/json', 'clientId': str(cwaClientId)})
+    response = cwaOppReq.json()
+    print(len(response))
+    print(response)
+    # for respEntry in response:
+        # if str(respEntry['name']) == 'Service Requests':
+        # print(respEntry)
+    # print(response)
+    if len(response) != pageSize:
+        break
+print('done')
+
